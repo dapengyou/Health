ca5b6b11386abd64edcf5cf7d81a6845e95b8bda
dapengyou <ladyztt@sina.com>
2018-04-13 14:52:39 +0800

    套餐详情悬停

--- app/src/main/java/com/test/health/ui/activity/SetMealActivity.java
+++ app/src/main/java/com/test/health/ui/activity/SetMealActivity.java
@@ -2,19 +2,21 @@ package com.test.health.ui.activity;
 
 import android.content.Intent;
 import android.graphics.Paint;
-import android.graphics.Point;
-import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.text.Spannable;
 import android.text.SpannableStringBuilder;
 import android.text.style.AbsoluteSizeSpan;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
-import com.test.baselibrary.base.BaseActivity;
 import com.test.baselibrary.base.TitleActivity;
+import com.test.health.MockData.MockData;
 import com.test.health.R;
+import com.test.health.ui.adapter.FirstFootAdapter;
 
 /**
  * 套餐页面
@@ -22,7 +24,11 @@ import com.test.health.R;
 public class SetMealActivity extends TitleActivity {
     private TextView mTvPric;//新价格
     private TextView mTvOldPrice;//老价格
+    private RecyclerView mRecyclerView;
+    private FirstFootAdapter mAdapter;
 
+    private CollapsingToolbarLayout mCollapsingToolbarLayout;
+    private Toolbar mToolbar;
     @Override
     protected int getContentResId() {
         return R.layout.activity_set_meal;
@@ -33,6 +39,12 @@ public class SetMealActivity extends TitleActivity {
         super.initView(savedInstanceState);
         mTvOldPrice = findViewById(R.id.tv_price_old);
         mTvPric = findViewById(R.id.tv_price);
+        mRecyclerView = findViewById(R.id.rv_list);
+
+//        mCollapsingToolbarLayout = findViewById(R.id.collapsing);
+//        mToolbar = findViewById(R.id.toolbar);
+
+//        setSupportActionBar(mToolbar);
 
     }
 
@@ -54,8 +66,14 @@ public class SetMealActivity extends TitleActivity {
         //不知道为什么使用 mTvPric.setText("￥"+textSpan); 显示不出效果
         mTvPric.setText(textSpan);
         mTvOldPrice.setText(text);
-        mTvOldPrice.setPaintFlags(Paint.STRIKE_THRU_TEXT_FLAG);
+        mTvOldPrice.setPaintFlags(Paint.STRIKE_THRU_TEXT_FLAG);//划斜线
 
+        //纵向RecycleView
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(mActivity);
+        linearLayoutManager.canScrollVertically();
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+        mAdapter = new FirstFootAdapter(MockData.getCommodityDatas(20));
+        mRecyclerView.setAdapter(mAdapter);
     }
 
     @Override
